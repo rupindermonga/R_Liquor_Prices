7f2e6740aa3a0344bc3e277e32ab84cdfdfc4d3b
Rupinder <rupindermonga@gmail.com>
2020-05-27 18:49:17 +0530

    Getting the Alcohl content with pricing

---
+++ app.R
@@ -0,0 +1,65 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(rsconnect)
+#rsconnect::deployApp('C:/Users/Rupinder/Desktop/RShiny/WebApps')
+
+
+rsconnect::setAccountInfo(name='rupindermonga',
+                          token='E7983CF5CD6F05979EBCF9AA61EF9925',
+                          secret='he3Jnu0JWu98gPTxI1/UYIQZ8ZL/st+y3t9P0QGp')
+
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      radioButtons("typeInput", "Product type",
+                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                   selected = "WINE"),
+      uiOutput("countryOutput")
+    ),
+    mainPanel(
+      plotOutput("coolplot"),
+      br(), br(),
+      tableOutput("results")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  })  
+  
+  filtered <- reactive({
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }    
+    
+    bcl %>%
+      filter(Price >= input$priceInput[1],
+             Price <= input$priceInput[2],
+             Type == input$typeInput,
+             Country == input$countryInput
+      )
+  })
+  
+  output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
+    ggplot(filtered(), aes(Alcohol_Content)) +
+      geom_histogram()
+  })
+  
+  output$results <- renderTable({
+    filtered()
+  })
+}
+
+shinyApp(ui = ui, server = server)
